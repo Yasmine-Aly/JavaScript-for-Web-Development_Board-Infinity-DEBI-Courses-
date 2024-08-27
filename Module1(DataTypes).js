12f8339f8a91c39b5e55c865f6d255783158aca0
Yasmine Ali <67972643+Yasmine-Aly@users.noreply.github.com>
2024-08-27 22:28:57 +0300

    Create Module1(DataTypes).js

---
+++ Module1(DataTypes).js
@@ -0,0 +1,15 @@
+let x = 5; //Number 
+const bigInt = 122345668687877585859895959595n; //big Integer
+console.log(typeof bigInt) //bigint
+let name ='Yasmine'; //string
+let isTrue = true; //boolean 
+let x = null; //null
+var age; //undefined
+console.log(age); //undefined
+//object
+let person ={
+  name: 'Yasmine',
+  age: 23
+}
+//symbol
+let id = symbol(id);
