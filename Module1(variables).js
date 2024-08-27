5744db798284a859323854122db4538b722f3af6
Yasmine Ali <67972643+Yasmine-Aly@users.noreply.github.com>
2024-08-27 22:18:26 +0300

    Create Module1(variables).js

---
+++ Module1(variables).js
@@ -0,0 +1,14 @@
+let name;
+name ='Yasmine';
+let count =0;
+count = 1;
+console.log(count); //1
+const pi = 3.14;
+pi = 4;
+console.log(pi); //error we can declared one more time after being constant
+var count_2 = 0
+if(true){
+  var count_2 = 1;
+  console.log(count_2);//1
+}
+console.log(count_2);//1
